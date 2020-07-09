9a466035dbd708c15d5af22fd2e52e23e0768a15
Piyush Shankar Tiwari <piyushtiwaru515@gmail.com>
2020-07-08 21:11:20 -0700

    back to previous

--- wordcount/urls.py
+++ wordcount/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.homepage, name='home'),
-    path('count/',views.count, name='count'),
+    path('counts/',views.count, name='count'),
     path('About_Me/', views.about, name='About_Me'),
 ]
